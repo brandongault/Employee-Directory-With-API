fb5cad8807cf6d809562d6c17d4601663b96f8be
Brandon <brandongault8@gmail.com>
2021-11-19 09:58:58 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -80,9 +80,18 @@ modalContainer.innerHTML = modalHTML;
 //---------- EVENT LISTENERS -------------
 //----------------------------------------
 
+gridContainer.addEventListener("click", e => {
+  if (e.target !== gridContainer) {
+    const card = e.target.closest(".card");
+    const index = card.getAttribute('data-index');
 
+    displayModal(index);
+  }
+});
 
-
+modalClose.addEventListener('click', () => {
+  overlay.classList.add("hidden");
+});
 
 }
 
