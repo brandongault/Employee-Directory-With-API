19cf8b375e2ea9ba76dc255928d1fbc49608ff39
Brandon <brandongault8@gmail.com>
2021-11-19 09:55:17 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -49,7 +49,42 @@ gridContainer.innerHTML = employeeHTML;
 
   }
 
+  //----------------------------------------
+  //---------- FUNCTIONS ---------
+  //----------------------------------------
 
+function displayModal(index) {
+
+let { name, dob, phone, email, location:
+  { city, street, state, postcode}, picture } = employees[index];
+
+let date = new Date(dob.date);
+
+const modalHTML = `
+  <img class="avatar" src="${picture.large}" />
+  <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    <hr />
+    <p>${phone}</p>
+    <p class="address">${street}, ${state} ${postcode}</p>
+    <p>Birthday:${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+  </div>
+`;
+
+overlay.classList.remove("hidden");
+modalContainer.innerHTML = modalHTML;
+
+//----------------------------------------
+//---------- EVENT LISTENERS -------------
+//----------------------------------------
+
+
+
+
+
+}
 
 
 
