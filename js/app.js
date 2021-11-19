27dc48b605958b2d3a52651dc11aaf6d5e9e1e6f
Brandon <brandongault8@gmail.com>
2021-11-19 09:46:15 -0500

    Adding JS

---
+++ js/app.js
@@ -0,0 +1,71 @@
+//-------------------------------------
+//---------- GLOBAL VARIABLES ---------
+//-------------------------------------
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name,
+                picture, email, location, phone, dob &noinfo
+                &nat=US`
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+
+//----------------------------------------
+//---------- FETCH DATA FROM API ---------
+//----------------------------------------
+fetch(ulrAPI)
+  .then(res => res.json())
+  .then(res => res.results)
+  .then(displayEmployees)
+  .catch(err => console.log(err))
+
+function displayEmployees(employeeData) {
+  employees = employeeData;
+
+//STORE THE EMPLOYEE HTML AS WE CREATE IT
+  let employeeHTML = '';
+
+//LOOP THROUGH EACH EMPLOYEE AND CREATE HTML MARKUP
+  employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+
+//TEMPLATE LITERALS MAKE THIS SO MUCH CLEANER
+    employeeHTML += `
+      <div class="card" data-index="${index}">
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+          <h2 class="name">${name.first} ${name.last}</h2>
+          <p class="email">${email}</p>
+          <p class="address">${city}</p>
+        </div>
+      </div>
+    `
+  });
+
+gridContainer.innerHTML = employeeHTML;
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  .
